6f71e6a98f890478fa54874a43abd8ad7da4ffee
PLATYPUSCO007 <william.enciso@cun.edu.co>
2024-05-16 19:20:12 -0500

    Project Blank

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 import './App.css'
 import Component01 from './components/component_01';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
         <Component01 />
       </div>
     </>
@@ -27,3 +15,4 @@ function App() {
 }
 
 export default App
+
